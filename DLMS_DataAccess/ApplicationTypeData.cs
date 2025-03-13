e65a8f12058048af432e6ccb5ee77cb6c1e3e0b5
marwandev <marwaneg@aucegypt.edu>
2025-03-13 11:25:49 +0200

    Create the manage application types form design

---
+++ DLMS_DataAccess/ApplicationTypeData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DLMS_DataAccess
+{
+    public class ApplicationTypeData
+    {
+        public static DataTable GetAllApplicationTypes()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT  [ApplicationTypeID]\r\n   " +
+                ",[ApplicationTypeTitle]\r\n    " +
+                ",[ApplicationFees]\r\n " +
+                "FROM [dvld].[dbo].[ApplicationTypes]";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                    dt.Columns["ApplicationTypeID"].ColumnName = "ID";
+                    dt.Columns["ApplicationTypeTitle"].ColumnName = "Title";
+                    dt.Columns["ApplicationFees"].ColumnName = "Fees";
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+        public static bool GetApplicationTypeById(int id, ref string title, ref double fees)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT  [ApplicationTypeID]\r\n   " +
+                ",[ApplicationTypeTitle]\r\n" +
+                ",[ApplicationFees]\r\n" +
+                "FROM [dvld].[dbo].[ApplicationTypes]\n" +
+                "Where ApplicationTypeID = @ApplicationTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ApplicationTypeID", id);
+
+            try
+            {
+                // The record was found
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    isFound = true;
+                    title = (string)reader["ApplicationTypeTitle"];
+                    fees = (double)reader["ApplicationFees"];
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+            }
+            catch (Exception)
+            {
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return isFound;
+        }
+
+        public static bool UpdateApplicationType(int id, string title, double fees)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  ApplicationTypes  
+                            set ApplicationTypeTitle = @ApplicationTypeTitle, 
+                                ApplicationTypeFees = @ApplicationTypeFees, 
+                                where ApplicationTypeID = @ApplicationTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ApplicationTypeID", id);
+            command.Parameters.AddWithValue("@ApplicationTypeFees", fees);
+            command.Parameters.AddWithValue("@ApplicationTypeTitle", title);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return (rowsAffected > 0);
+        }
+    }
+}
