352c98520924880dd556a261bb270bf90b7d969f
marwandev <marwaneg@aucegypt.edu>
2025-03-20 11:18:05 +0200

    Add count to the local DL Applications form

--- DLMS_DataAccess/LocalDLApplicationData.cs
+++ DLMS_DataAccess/LocalDLApplicationData.cs
@@ -82,7 +82,45 @@ namespace DLMS_DataAccess
             "LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID AS 'L.D.L.APP.ID',\r\n        " +
             "ClassName AS 'Driving Class',\r\n        " +
             "NationalNo AS 'National No.',\r\n        " +
-            "CONCAT(People.FirstName, ' ', People.SecondName, ' ', People.ThirdName, ' ', People.LastName) AS FullName,\r\n        " +
+            "CONCAT(People.FirstName, ' ', People.SecondName, ' ', People.ThirdName, ' ', People.LastName) AS 'Full Name',\r\n        " +
+            "[ApplicationDate] AS 'Application Date',\r\n\r\n        " +
+            "(SELECT COUNT(*) \r\n         " +
+            "FROM Tests \r\n         " +
+            "JOIN TestAppointments ON Tests.TestAppointmentID = TestAppointments.TestAppointmentID\r\n         " +
+            "WHERE TestAppointments.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID\r\n           " +
+            "AND Tests.TestResult = 1) AS 'Passed Tests',\r\n\r\n        " +
+            "CASE\r\n            " +
+            "WHEN [ApplicationStatus] = 0 THEN 'Cancelled'\r\n            " +
+            "WHEN [ApplicationStatus] = 1 THEN 'New'\r\n            " +
+            "WHEN [ApplicationStatus] = 2 THEN 'In Progress'\r\n            " +
+            "WHEN [ApplicationStatus] = 3 THEN 'Complete'\r\n        " +
+            "END AS 'Status'\r\n        \r\n    " +
+            "FROM LocalDrivingLicenseApplications \r\n    " +
+            "JOIN [dvld].[dbo].[Applications] \r\n        " +
+            "ON LocalDrivingLicenseApplications.ApplicationID = Applications.ApplicationID\r\n    " +
+            "JOIN People \r\n        ON People.PersonID = Applications.ApplicantPersonID\r\n    " +
+            "LEFT JOIN LicenseClasses \r\n        " +
+            "ON LicenseClasses.LicenseClassID = LocalDrivingLicenseApplications.LicenseClassID\r\n    " +
+            "LEFT JOIN TestAppointments \r\n        " +
+            "ON TestAppointments.LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID\r\n    " +
+            "LEFT JOIN Tests \r\n        " +
+            "ON Tests.TestAppointmentID = TestAppointments.TestAppointmentID\r\n\t\tGROUP BY \r\n    " +
+            "LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID,\r\n    " +
+            "ClassName,\r\n    " +
+            "NationalNo,\r\n    " +
+            "People.FirstName, " +
+            "People.SecondName, " +
+            "People.ThirdName, " +
+            "People.LastName,\r\n    " +
+            "ApplicationDate,\r\n    " +
+            "ApplicationStatus\r\n) r1";
+
+        private static readonly string GetAllLocalApplicationsCountQuery = "SELECT Count (*) FROM (\r\n    " +
+            "SELECT \r\n        " +
+            "LocalDrivingLicenseApplications.LocalDrivingLicenseApplicationID AS 'L.D.L.APP.ID',\r\n        " +
+            "ClassName AS 'Driving Class',\r\n        " +
+            "NationalNo AS 'National No.',\r\n        " +
+            "CONCAT(People.FirstName, ' ', People.SecondName, ' ', People.ThirdName, ' ', People.LastName) AS 'Full Name',\r\n        " +
             "[ApplicationDate] AS 'Application Date',\r\n\r\n        " +
             "(SELECT COUNT(*) \r\n         " +
             "FROM Tests \r\n         " +
@@ -148,7 +186,7 @@ namespace DLMS_DataAccess
         private static string GetFilterConditionText(string filterkeyWord)
         {
             string filterCondition = CurrentFilter == "L.D.L.APP.ID" ||
-                CurrentFilter == "National No." || CurrentFilter == "First Name" ?
+                CurrentFilter == "National No." || CurrentFilter == "Full Name" ?
                 $"\"{CurrentFilter}\" like '%{filterkeyWord}%'" :
                 $"\"{CurrentFilter}\" = '{filterkeyWord}'";
             return filterCondition.Length > 0 ? "\nwhere " + filterCondition : "";
@@ -180,6 +218,66 @@ namespace DLMS_DataAccess
             }
 
             return dt;
+        }
+
+        public static int GetAllLocalDLApplicationsCount()
+        {
+            int count = 0;
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllLocalApplicationsCountQuery;
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                    count = (int)(dt.Rows[0][0] ?? null);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return count;
+        }
+
+        public static int GetFilteredLocalDLApplicationsCount(string filterkeyWord)
+        {
+            int count = 0;
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllLocalApplicationsCountQuery + GetFilterConditionText(filterkeyWord);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                    count = (int)(dt.Rows[0][0] ?? null);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return count;
         }
     }
 }
