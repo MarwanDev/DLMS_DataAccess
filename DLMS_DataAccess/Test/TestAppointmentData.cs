518907b1ac1b75990a92e1ab42a5273da12326d7
marwandev <marwaneg@aucegypt.edu>
2025-03-25 13:15:09 +0200

    Add the foundation for test appointments

--- DLMS_DataAccess/Test/TestAppointmentData.cs
+++ DLMS_DataAccess/Test/TestAppointmentData.cs
@@ -1,13 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DLMS_DataAccess
 {
-    internal class TestAppointmentData
+    public class TestAppointmentData : Data
     {
+        public static int AddNewTestAppointment(int testTypeId, int localDLApplicationId, DateTime appointmentDate, 
+            decimal paidFees, int createdByUserId, bool isLocked)
+        {
+            int testAppointmentID = -1;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = @"INSERT INTO TestAppointments (TestTypeID, LocalDrivingLicenseApplicationID, AppointmentDate, PaidFees, CreatedByUserID, IsLocked)
+                            VALUES (@TestTypeID, @LocalDrivingLicenseApplicationID, @AppointmentDate, @PaidFees, @CreatedByUserID, @IsLocked);
+                             SELECT SCOPE_IDENTITY();";
 
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestTypeID", testTypeId);
+            command.Parameters.AddWithValue("@LocalDrivingLicenseApplicationID", localDLApplicationId);
+            command.Parameters.AddWithValue("@AppointmentDate", appointmentDate);
+            command.Parameters.AddWithValue("@PaidFees", paidFees);
+            command.Parameters.AddWithValue("@CreatedByUserID", createdByUserId);
+            command.Parameters.AddWithValue("@IsLocked", isLocked);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    testAppointmentID = insertedID;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return testAppointmentID;
+        }
     }
 }
