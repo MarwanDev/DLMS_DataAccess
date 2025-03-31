236ae995627c1544ebcd604b60056e09a54f7c99
marwandev <marwaneg@aucegypt.edu>
2025-03-31 09:35:33 +0200

    Add the diver in the data access and business layers

--- DLMS_DataAccess/Person/DriverData.cs
+++ DLMS_DataAccess/Person/DriverData.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DLMS_DataAccess.Person
 {
@@ -10,7 +7,35 @@ namespace DLMS_DataAccess.Person
     {
         public static int AddNewDriver(int personId, int createdByUserId, DateTime createdDate)
         {
+            int licenceId = -1;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = @"INSERT INTO Drivers (PersonId, CreatedByUserId, CreatedDate)
+                            VALUES (@PersonId, @CreatedByUserId, @CreatedDate);
+                            SELECT SCOPE_IDENTITY();"
+            ;
 
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@PersonId", personId);
+            command.Parameters.AddWithValue("@CreatedByUserId", createdByUserId);
+            command.Parameters.AddWithValue("@CreatedDate", createdDate);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    licenceId = insertedID;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return licenceId;
         }
     }
 }
