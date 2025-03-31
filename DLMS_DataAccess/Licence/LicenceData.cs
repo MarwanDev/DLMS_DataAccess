beeba5b40e4b75b402ba8434acc7fce908dd2b6b
marwandev <marwaneg@aucegypt.edu>
2025-03-31 04:20:35 +0200

    Add the licence and driver to the data access layer and modify folder structure

---
+++ DLMS_DataAccess/Licence/LicenceData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DLMS_DataAccess.Licence
+{
+    public class LicenceData
+    {
+        public static int AddNewLicence(int applicationId, int driverId, int licenceClassId,
+            DateTime issueDate, bool isActive, string notes,
+            DateTime expirationDate, decimal paidFees, int createdByUserId, byte issueReason)
+        {
+            int licenceId = -1;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = @"INSERT INTO Licenses (ApplicationID, DriverId, LicenseClassID, IssueDate,
+                            IsActive, Notes, ExpirationDate, PaidFees, CreatedByUserId, IssueReason)
+                            VALUES (@ApplicationID, @DriverId, @LicenseClassID, @IssueDate,
+                            @IsActive, @Notes, @ExpirationDate, @PaidFees, @CreatedByUserId, @IssueReason);
+                            SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ApplicationID", applicationId);
+            command.Parameters.AddWithValue("@DriverId", driverId);
+            command.Parameters.AddWithValue("@LicenseClassID", licenceClassId);
+            command.Parameters.AddWithValue("@IssueDate", issueDate);
+            command.Parameters.AddWithValue("@IsActive", isActive);
+            command.Parameters.AddWithValue("@Notes", notes);
+            command.Parameters.AddWithValue("@ExpirationDate", expirationDate);
+            command.Parameters.AddWithValue("@PaidFees", paidFees);
+            command.Parameters.AddWithValue("@CreatedByUserId", createdByUserId);
+            command.Parameters.AddWithValue("@IssueReason", issueReason);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    licenceId = insertedID;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return licenceId;
+        }
+    }
+}
---
+++ DLMS_DataAccess/Person/DriverData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMS_DataAccess.Person
+{
+    public class DriverData
+    {
+        public static int AddNewDriver(int personId, int createdByUserId, DateTime createdDate)
+        {
+
+        }
+    }
+}
