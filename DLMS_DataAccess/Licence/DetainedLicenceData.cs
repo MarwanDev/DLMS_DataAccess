398c779e638bcc6c4651aecb32dbef281fe5f2b1
marwandev <marwaneg@aucegypt.edu>
2025-04-05 14:53:38 +0200

    Add the list of the detained licences with filters

--- DLMS_DataAccess/Licence/DetainedLicenceData.cs
+++ DLMS_DataAccess/Licence/DetainedLicenceData.cs
@@ -1,12 +1,11 @@
 ﻿
 using System.Data.SqlClient;
 using System;
-using System.Net;
-using System.Security.Policy;
+using System.Data;
 
 namespace DLMS_DataAccess.Licence
 {
-    public class DetainedLicenceData
+    public class DetainedLicenceData : Data
     {
         public static bool IsLicenceDetained(int licenceId)
         {
@@ -208,6 +207,164 @@ namespace DLMS_DataAccess.Licence
                 connection.Close();
             }
             return isFound;
+        }
+
+        private static readonly string GetAllDetainedLicencesQuery = "Select * from (" +
+            "\r\n\r\nSELECT [DetainID] as 'D.ID'\r\n      " +
+            ",DetainedLicenses.LicenseID as 'L.ID'\r\n      " +
+            ",[DetainDate] as 'D.Date'\r\n      " +
+            ",[FineFees] as 'Fine Fees'\r\n      " +
+            ",[IsReleased] as 'Is Released'\r\n      " +
+            ",[ReleaseDate] as 'Release Date'\r\n      " +
+            ",[NationalNo] as 'N.No'\r\n      " +
+            ",CONCAT(People.FirstName, ' ', People.SecondName, ' ', People.ThirdName, ' ', People.LastName) AS 'Full Name'\r\n      " +
+            ",[ReleaseApplicationID] as 'Release App.ID'\r\n  " +
+            "FROM [dvld].[dbo].[DetainedLicenses]\r\n  " +
+            "join Licenses on DetainedLicenses.LicenseID = Licenses.LicenseID\r\n  " +
+            "join Drivers on Drivers.DriverID = Licenses.DriverID\r\n  " +
+            "join People on People.PersonID = Drivers.PersonID\r\n  " +
+            ")\r\n  R1\n";
+
+        private static readonly string GetAllDetainedLicencesCountQuery = "Select Count (*) from (" +
+            "\r\n\r\nSELECT [DetainID] as 'D.ID'\r\n      " +
+            ",DetainedLicenses.LicenseID as 'L.ID'\r\n      " +
+            ",[DetainDate] as 'D.Date'\r\n      " +
+            ",[FineFees] as 'Fine Fees'\r\n      " +
+            ",[IsReleased] as 'Is Released'\r\n      " +
+            ",[ReleaseDate] as 'Release Date'\r\n      " +
+            ",[NationalNo] as 'N.No'\r\n      " +
+            ",CONCAT(People.FirstName, ' ', People.SecondName, ' ', People.ThirdName, ' ', People.LastName) AS 'Full Name'\r\n      " +
+            ",[ReleaseApplicationID] as 'Release App.ID'\r\n  " +
+            "FROM [dvld].[dbo].[DetainedLicenses]\r\n  " +
+            "join Licenses on DetainedLicenses.LicenseID = Licenses.LicenseID\r\n  " +
+            "join Drivers on Drivers.DriverID = Licenses.DriverID\r\n  " +
+            "join People on People.PersonID = Drivers.PersonID\r\n  " +
+            ")\r\n  R1\n";
+
+        public static DataTable GetAllDetainedLicences()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllDetainedLicencesQuery + SortingCondition;
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+        public static int GetAllDetainedLicencesCount()
+        {
+            int count = 0;
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllDetainedLicencesCountQuery;
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                    count = (int)(dt.Rows[0][0] ?? null);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return count;
+        }
+
+        public static int GetFilteredDetainedLicencesCount(string filterkeyWord)
+        {
+            int count = 0;
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllDetainedLicencesCountQuery + GetFilterConditionText(filterkeyWord);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                    count = (int)(dt.Rows[0][0] ?? null);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return count;
+        }
+
+        public static string CurrentFilter;
+
+        private static string GetFilterConditionText(string filterkeyWord)
+        {
+            string filterCondition = CurrentFilter != "Is Released" ?
+                $"\"{CurrentFilter}\" like '%{filterkeyWord}%'" :
+                $"\"{CurrentFilter}\" = '{filterkeyWord}'";
+            return filterCondition.Length > 0 ? "\nwhere " + filterCondition : "";
+        }
+
+        public static DataTable GetFilteredDetainedLicences(string filterkeyWord)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = GetAllDetainedLicencesQuery + GetFilterConditionText(filterkeyWord) + SortingCondition;
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    dt.Load(reader);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
         }
     }
 }
